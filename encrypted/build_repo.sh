de158745825d0bcc28b198f8f0cd55fd0f7c18b0
Pellegrino Prevete <pellegrinoprevete@gmail.com>
2022-06-08 05:16:10 +0000

    edit systemd-boot entries

--- encrypted/build_repo.sh
+++ encrypted/build_repo.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 
+_build_pkg() {
+    local _pwd="$(pwd)"
+    local _pkgname="${1}"
+    cd "${_pkgname}"
+    makepkg
+    mv "${_pkgname}"*".pkg.tar."* "${_server}"
+    cd "${_pwd}"
+}
+
 _build_repo() {
     local _profile="encrypted"
     local _pwd="$(pwd)"
     local _server="/tmp/archiso-profiles/${_profile}/any"
     rm -rf repo "${_server}" && mkdir -p repo "${_server}"
     cd repo
-    local _pkgname="mkinitcpio-archiso-encryption"
-    git clone "https://aur.archlinux.org/${_pkgname}"
-    cd "${_pkgname}"
-    makepkg
-    mv "${_pkgname}"*"any.pkg.tar."* "${_server}"
+    _build_pkg "cryptsetup-nested-cryptkey"
+    _build_pkg "mkinitcpio-archiso-encryption"
     cd "${_server}"
-    repo-add "${_profile}.any.db.tar.gz" *"any.pkg.tar."*
+    repo-add "${_profile}.db.tar.gz" *".pkg.tar."*
     cd "${_pwd}"
     rm -rf repo
 }
