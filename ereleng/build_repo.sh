b898416a142b89dd503a38c4755fd76e08fab874
Pellegrino Prevete <pellegrinoprevete@gmail.com>
2022-06-08 17:56:14 +0000

    wrong package

--- ereleng/build_repo.sh
+++ ereleng/build_repo.sh
@@ -14,13 +14,14 @@ _build_pkg() {
 }
 
 _build_repo() {
-    local _profile="encrypted"
+    local _profile
+    _profile=$(dirname ./)
     local _server="/tmp/archiso-profiles/${_profile}"
     rm -rf repo "${_server}" && mkdir -p repo "${_server}"
     cd repo || exit
     gpg --recv-keys "D9B0577BD93E98FC" # cryptsetup
     _build_pkg "cryptsetup-nested-cryptkey"
-    _build_pkg "mkinitcpio-archiso"
+    _build_pkg "mkinitcpio-archiso-encryption"
     cd ..
     rm -rf repo
 }
