bad221fa0a306ddbe77d545024ace84613364543
Pellegrino Prevete <pellegrinoprevete@gmail.com>
2022-06-16 01:54:29 +0000

    add gdm and gnome-shell

--- desktop/profiledef.sh
+++ desktop/profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux-ebaseline"
+iso_name="archlinux-desktop"
 iso_label="ARCH_$(date +%Y%m)"
 iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux baseline"
+iso_application="Arch Linux Desktop"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso' 'keys')
