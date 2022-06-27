16cc626274987e71a8ff6608117db2c3bda6e208
Pellegrino Prevete <pellegrinoprevete@gmail.com>
2022-06-27 22:58:43 +0000

    update colab script

--- desktop/colab.py
+++ desktop/colab.py
@@ -1,8 +1,10 @@
 #@title Build `releng`
-#@markdown Down run this on your local Ubuntu, you will break it
+#@markdown Down run this on your local Ubuntu, you will break it.
+#@markdown The first run takes probably half an hour, so take a coffee and don't
+#@markdown close the page
 
 def install_deps():
-  !sudo apt install autopoint asciidoc bsdtar build-essential cmake dosfstools fakeroot gnulib help2man intltool libarchive-dev libtool libzstd-dev m4 make mtools git grub2 python-pip shellcheck squashfs-tools texinfo zstd #>/dev/null 2>&1
+  !sudo apt install autopoint asciidoc bsdtar build-essential cmake dosfstools fakeroot git gnulib grub2 help2man intltool libtool libzstd-dev m4 make mtools python-pip shellcheck squashfs-tools texinfo zstd #>/dev/null 2>&1
   !sudo pip install meson ninja >/dev/null 2>&1
 
 def install_arch_install_scripts():
@@ -17,12 +19,12 @@ def install_asp():
   !sudo install -Dm644 asp/LICENSE "/usr/share/licenses/asp/LICENSE" >/dev/null 2>&1
 
 def install_pacman():
-  # !rm -rf pacman
+  !rm -rf pacman
   !mkdir /etc/pacman.d
   !git clone https://gitlab.archlinux.org/tallero/archiso archiso >/dev/null 2>&1
   !git clone https://gitlab.archlinux.org/pacman/pacman >/dev/null 2>&1
   
-  !cd pacman && meson --prefix=/usr --buildtype=plain -Ddoc=disabled -Ddoxygen=disabled -Dscriptlet-shell=/usr/bin/bash -Dldconfig=/usr/bin/ldconfig build >/dev/null 2>&1
+  !cd pacman && meson --prefix=/usr --buildtype=plain -Ddoc=disabled -Ddoxygen=disabled -Dscriptlet-shell=/usr/bin/bash -Dldconfig=/usr/bin/ldconfig build #>/dev/null 2>&1
   !cd pacman && meson compile -C build >/dev/null 2>&1
   !cd pacman && sudo DESTDIR="/" meson install -C build >/dev/null 2>&1
 
@@ -44,23 +46,21 @@ def install_archiso():
   !cd archiso && make DESTDIR="" PREFIX='/usr' install >/dev/null 2>&1
 
 def install_zstd():
-  #!rm -rf zstd-1.5.2
-  #!git clone https://github.com/facebook/zstd >/dev/null 2>&1
+  !rm -rf zstd-1.5.2
   !wget https://github.com/facebook/zstd/releases/download/v1.5.2/zstd-1.5.2.tar.gz
   !tar -xf zstd-1.5.2.tar.gz
-  #!cd zstd-1.5.2 && ./configure --prefix=/usr
-  !cd zstd-1.5.2 && CFLAGS+=' -ffat-lto-objects' && CXXFLAGS+=' -ffat-lto-objects' cmake -S build/cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=None -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=lib -DZSTD_BUILD_CONTRIB=ON -DZSTD_BUILD_STATIC=OFF -DZSTD_BUILD_TESTS=OFF -DZSTD_PROGRAMS_LINK_SHARED=ON && cmake --build build
-  !cd zstd-1.5.2 && DESTDIR="${pkgdir}" cmake --install build
-  !cd zstd-1.5.2 && ln -sf /usr/bin/zstd "${pkgdir}/usr/bin/zstdmt"
+  !cd zstd-1.5.2 && sed '/build static library to build tests/d' -i build/cmake/CMakeLists.txt
+  !cd zstd-1.5.2 && sed 's/libzstd_static/libzstd_shared/g' -i build/cmake/tests/CMakeLists.txt
+  !cd zstd-1.5.2 && CFLAGS+=' -ffat-lto-objects' && CXXFLAGS+=' -ffat-lto-objects' cmake -S build/cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=None -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=/usr/lib -DZSTD_BUILD_CONTRIB=ON -DZSTD_BUILD_STATIC=ON -DZSTD_BUILD_TESTS=OFF -DZSTD_PROGRAMS_LINK_SHARED=ON && cmake --build build
+  !cd zstd-1.5.2 && DESTDIR="/" cmake --install build
+  #!cd zstd-1.5.2 && ln -sf /usr/bin/zstd "/usr/bin/zstdmt"
   #!cd zstd-1.5.2 && make >/dev/null 2>&1
   #!cd zstd-1.5.2 && sudo DESTDIR="/" PREFIX="/usr" make install >/dev/null 2>&1
   #!cp -r "/content/zstd/lib" /usr
 
 def install_gettext():
-  # !git clone https://git.savannah.gnu.org/git/gettext.git
   !wget https://ftp.gnu.org/gnu/gettext/gettext-0.21.tar.xz >/dev/null 2>&1
   !tar xf gettext-0.21.tar.xz >/dev/null 2>&1
-  # !cd gettext-0.21 && git checkout eb83efc3b58d690d444da0b82c3ecf36a54f28ac
   !cd gettext-0.21 && autoreconf -i >/dev/null 2>&1
   !cd gettext-0.21 && ./configure --prefix=/usr >/dev/null 2>&1
   !cd gettext-0.21 && make >/dev/null 2>&1
@@ -74,7 +74,7 @@ def install_autoconf():
   !cd autoconf && sudo DESTDIR="/" PREFIX="/usr" make install >/dev/null 2>&1
 
 def install_tar():
-  !apt-get build-dep tar >/dev/null 2>&1
+  # !apt-get build-dep tar >/dev/null 2>&1
   !wget https://ftp.gnu.org/gnu/tar/tar-1.34.tar.xz >/dev/null 2>&1
   !tar -xf tar-1.34.tar.xz >/dev/null 2>&1
   # !git clone https://git.savannah.gnu.org/git/tar.git
@@ -92,11 +92,13 @@ def install_cmake():
   !cd cmake-3.23.2 && sudo DESTDIR="/" PREFIX="/usr" make install #>/dev/null 2>&1
 
 def install_libarchive():
+  !rm -rf libarchive-3.6.1
   !wget https://github.com/libarchive/libarchive/releases/download/v3.6.1/libarchive-3.6.1.tar.xz
   !tar xf libarchive-3.6.1.tar.xz
-  !cd libarchive-3.6.1 && ./configure --prefix=/usr
-  !cd libarchive-3.6.1 && make
-  !cd libarchive-3.6.1 && DESTDIR="/" PREFIX="/usr" sudo make install 
+  !ls libarchive-3.6.1
+  !cd libarchive-3.6.1 && cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
+  !cd libarchive-3.6.1 && cmake --build build
+  !cd libarchive-3.6.1 && DESTDIR="/" PREFIX="/usr" sudo cmake --install build
 
 def install_reflector():
   !rm -rf reflector
@@ -126,18 +128,18 @@ def build_ereleng():
   !su user -c "cd archiso-profiles/ereleng && bash build_repo.sh"
   #!cd archiso-profiles/desktop && bash build_repo.sh
 
-#install_deps()
-#install_arch_install_scripts()
-#install_asp()
-#install_zstd()
-#install_gettext()
-#install_autoconf()
-#install_tar()
+install_deps()
+install_arch_install_scripts()
+# install_asp()
+install_zstd()
+install_gettext()
+install_autoconf()
+install_tar()
 install_cmake()
-#install_libarchive()
-#install_pacman()
-#install_archiso()
-#install_reflector()
-#build_releng()
+install_libarchive()
+install_pacman()
+install_archiso()
+# install_reflector()
+build_releng()
 #build_ereleng()
 
