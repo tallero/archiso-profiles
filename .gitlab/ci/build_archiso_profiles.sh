e32a0d0e95f4c69c904c05ff05e7984531b0e5c4
Pellegrino Prevete <pellegrinoprevete@gmail.com>
2022-09-26 17:45:54 +0000

    update ci script

--- .gitlab/ci/build_archiso_profiles.sh
+++ .gitlab/ci/build_archiso_profiles.sh
@@ -174,26 +174,27 @@ create_ephemeral_keys() {
   local _gen_key
   local _gen_key_options=('ephemeral') _gpg_options=() _openssl_options=()
   _gen_key="$(pwd)/.gitlab/ci/gen_key.sh"
+  [ -e "${_gen_key}" ] || _gen_key="mkarchisogenkey"
   print_section_start "ephemeral_pgp_key" "Creating ephemeral PGP key"
   local gnupg_homedir="${tmpdir}/.gnupg"
   signature_info
-  _gpg_options=("${gnupg_homedir}"
-                "${sig_email}"
-                "${sig_unit}"
-                "${sig_comment}")
+  _gpg_options+=("${gnupg_homedir}"
+                 "${sig_email}"
+                 "${sig_unit}"
+                 "${sig_comment}")
   "${_gen_key}" "${_gen_key_options[@]}" 'gpg' "${_gpg_options[@]}"
   create_ephemeral_pgp_key
   print_section_end "ephemeral_pgp_key"
   print_section_start "ephemeral_codesigning_key" "Creating ephemeral codesigning key"
   codesigning_dir="${tmpdir}/.codesigning/"
-  _openssl_options=("${codesigning_dir}"
-                    "${sig_country}"
-                    "${sig_state}"
-                    "${sig_city}"
-                    "${sig_org}"
-                    "${sig_unit}"
-                    "${sig_domain}")
-  "${_gen_key}" "${_gen_key_options[@]} 'openssl' "${_openssl_options[@]}"
+  _openssl_options+=("${codesigning_dir}"
+                     "${sig_country}"
+                     "${sig_state}"
+                     "${sig_city}"
+                     "${sig_org}"
+                     "${sig_unit}"
+                     "${sig_domain}")
+  "${_gen_key}" "${_gen_key_options[@]}" 'openssl' "${_openssl_options[@]}"
   print_section_end "ephemeral_codesigning_key"
 }
 
